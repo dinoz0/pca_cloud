64d67f6ede6328088ecbadc0c712da916c8b3a05
dinoz0 <linuxwindows@LAPTOP-QFNIK8D3.localdomain>
2022-07-13 15:09:04 +0200

    fafaf

--- automatisation/auto.sh
+++ automatisation/auto.sh
@@ -11,9 +11,14 @@ read
 
 
 docker login
-docker build -t dinoz/pca_cloud ../backend/srv_ctn 
-docker build -t dinoz/pca_cloud ../frontend/cocktail 
-docker push dinoz/pca_cloud 
+docker build -t dinoz/pca_cloud_ctn ../backend/srv_ctn 
+docker build -t dinoz/pca_cloud_user ../backend/srv_user
+docker build -t dinoz/pca_cloud_data ../backend/srv_data
+docker build -t dinoz/pca_cloud_front ../frontend/cocktail 
+docker push dinoz/pca_cloud_ctn
+docker push dinoz/pca_cloud_user
+docker push dinoz/pca_cloud_data
+docker push dinoz/pca_cloud_front
 
 kubectl apply -f ../mysql_dep_secret.yml
 kubectl apply -f ../mysql-configmap.yml
